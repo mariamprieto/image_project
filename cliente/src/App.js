05fc5d5593b5eb6cfbcdb757b0deda0813e7d4a6
mariamprieto <mariaprieto@byui.edu>
2022-07-27 15:25:14 -0400

    update work

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,9 +1,54 @@
-import React, { Fragment} from 'react';
+import React, { Fragment, useDebugValue, useState } from 'react';
 
 function App() {
+
+  const [file, setFile] = useState(null)
+
+  const selectHandler = e => {
+    setFile(e.target.files[0])
+  }
+  const sendHandler = () => {
+    if (!file) {
+      alert('Você deve selecionar um arquivo')
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('image', file)
+    fetch('http://localhost:9000/images/post', {
+      method: 'POST',
+      body: formData
+    })
+      .then(res => res.text())
+      .then(res => console.log(res))
+      .catch(err => {
+        console.error(err)
+      })
+    document.getElementById('fileInput').value = null
+    setFile(null)
+
+  }
+
   return (
     <Fragment>
-      
+      <nav className='navbar navbar-dark bg-dark'>
+        <div className='container'>
+          <a href='#!' className='navbar-brand'> Image App</a>
+        </div>
+      </nav>
+      <div className='container mt-5'>
+        <div className='card p-3'>
+          <div className='row'>
+            <div className='col-10'>
+              <input id="fileInput" onChange={selectHandler} className='form-control' type='file' />
+            </div>
+            <div className='col-2'>
+              <button onClick={sendHandler} type='button' className='btn btn-primary col-12'> Upload</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </Fragment>
   );
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const multer = require('multer')
 const router = express.Router()
+const path= require ('path')
 
-//server routes
+const diskStorage = multer.diskStorage({
+    destination: path.join(__dirname, '../images'),
+    filename: (req, file, cb) => {
+        cb (null,Date.now() + '-monkeywit-'+ file.originalname)
+    }
+})
+
+const fileUpload = multer({
+    storage:diskStorage
+}).single('image')
+    
+
 router.get('/', (req, res) => {
     res.send ('Welcome to image page')
-} )
+})
+
+router.post('./images/post',fileUpload ,(req, res) => {
+    console.log(req.file)
+})
 
 module.exports=router
