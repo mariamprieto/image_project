b3ce56549fc9b4e076df00ec7db8f2f39671dee8
mariamprieto <mariaprieto@byui.edu>
2022-07-27 22:12:38 -0400

    update

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const multer = require('multer')
 const path = require('path')
-const router = express.Router()
 const fs = require('fs')
 
+const router = express.Router()
 
-const diskStorage = multer.diskStorage({
+const diskstorage = multer.diskStorage({
     destination: path.join(__dirname, '../images'),
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-monkeywit-' + file.originalname)
@@ -13,31 +13,29 @@ const diskStorage = multer.diskStorage({
 })
 
 const fileUpload = multer({
-    storage: diskStorage
+    storage: diskstorage
 }).single('image')
 
-
 router.get('/', (req, res) => {
-    res.send('Welcome to image page')
+    res.send('Welcome to my image app')
 })
 
 router.post('/images/post', fileUpload, (req, res) => {
 
     req.getConnection((err, conn) => {
         if (err) return res.status(500).send('server error')
-        
+
         const type = req.file.mimetype
-
         const name = req.file.originalname
-
-        const data = fs.readFileSync(path.join(__dirname, '../images' + req.file.filename))
+        const data = fs.readFileSync(path.join(__dirname, '../images/' + req.file.filename))
 
         conn.query('INSERT INTO image set ?', [{ type, name, data }], (err, rows) => {
             if (err) return res.status(500).send('server error')
+
             res.send('image saved!')
         })
-
     })
+
 })
 
 module.exports = router
